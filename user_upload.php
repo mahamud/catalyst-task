5c1696bcd81a769b0184758e8944c9c00f94b4ff
Mahamud Shahjahan <mahamud@eightcap.com>
2019-09-06 00:16:06 +1000

    moving validation to a method

--- user_upload.php
+++ user_upload.php
@@ -10,34 +10,10 @@ sleep(2);
 echo 'Options passed are : '.PHP_EOL.PHP_EOL;
 
 $arguments = processArguments($argv);
-
 var_dump(print_r($arguments, true));
-
-// Validate arguments and display error message accordingly
-if (is_array($arguments)) {
-    if (sizeof($arguments) > ARGUMENT_SIZE) {
-        displayErrorMessage("Error: Invalid number of arguments passed. Execute php user_upload --help for details.", true);
-    }else{
-        //Get all argument keys
-        $argumentArrayKeys = array_keys($arguments);
-        $result = array_diff($argumentArrayKeys, ARGUMENTKEYS); //Validate against valid argument keys
-        if(is_array($result) == false || sizeof($result) > 0){
-            displayErrorMessage("Error: Invalid argument key passed. Execute php user_upload --help for details.", true);
-        }
-
-        //Validate against dry_run and file combination
-        if(in_array('dry_run', $argumentArrayKeys) && in_array('file', $argumentArrayKeys) == false){
-            if(empty($argumentArrayKeys['file'])){
-                displayErrorMessage("Error: Dry runs can only be initiated when a file name is provided. Please provide a file name.", true);
-            }
-        }
-
-    }
-}
-
-
+validateArguments($arguments);
 $db = getDatabaseConnection(new PgSqlConnection());
 
-
+// End the Execution here if not stopped before.
 endScript();
 

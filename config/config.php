a742e4698985cada13bf0d133a13f1000a4771c7
Mahamud Shahjahan <mahamud@eightcap.com>
2019-09-05 16:17:57 +1000

    argument validation improved

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
 
+define('ARGUMENT_SIZE', 6); //Argument Size
+
 //Valid argument keys
 const ARGUMENTKEYS = array('file','create_table','dry_run', 'u', 'p', 'h');
--- user_upload.php
+++ user_upload.php
@@ -13,15 +13,25 @@ $arguments = processArguments($argv);
 var_dump(print_r($arguments, true));
 
 // Validate arguments and display error message accordingly
-if(is_array($arguments) == false || sizeof($arguments) < 1 || sizeof($arguments) > 6){
-    displayErrorMessage("Error: Invalid arguments passed. Execute php user_upload --help for details.", true);
-}
+if (is_array($arguments)) {
+    if (sizeof($arguments) > ARGUMENT_SIZE) {
+        displayErrorMessage("Error: Invalid number of arguments passed. Execute php user_upload --help for details.", true);
+    }else{
+        //Get all argument keys
+        $argumentArrayKeys = array_keys($arguments);
+        $result = array_diff($argumentArrayKeys, ARGUMENTKEYS); //Validate against valid argument keys
+        if(is_array($result) == false || sizeof($result) > 0){
+            displayErrorMessage("Error: Invalid argument key passed. Execute php user_upload --help for details.", true);
+        }
 
-//Get all argument keys
-$argumentArrayKeys = array_keys($arguments);
-$result = array_diff($argumentArrayKeys, ARGUMENTKEYS); //Validate against valid argument keys
-if(is_array($result) == false || sizeof($result) > 0){
-    displayErrorMessage("Error: Invalid argument key passed. Execute php user_upload --help for details.", true);
+        //Validate against dry_run and file combination
+        if(in_array('dry_run', $argumentArrayKeys) && in_array('file', $argumentArrayKeys) == false){
+            if(empty($argumentArrayKeys['file'])){
+                displayErrorMessage("Error: Dry runs can only be initiated when a file name is provided. Please provide a file name.", true);
+            }
+        }
+
+    }
 }
 
 //var_dump(print_r($result), true);
